43a590cfa665e5cea4d114272a42e4fd973a06c8
Tarun Chawla <37270001+tarunchawla28@users.noreply.github.com>
2018-12-25 18:36:35 +0530

    Update BookController.java

--- src/main/java/np/com/mshrestha/bookstore/controller/BookController.java
+++ src/main/java/np/com/mshrestha/bookstore/controller/BookController.java
@@ -6,7 +6,7 @@ import np.com.mshrestha.bookstore.model.Book;
 import np.com.mshrestha.bookstore.service.BookService;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Controller;;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
